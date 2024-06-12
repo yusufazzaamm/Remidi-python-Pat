5ca9fa90c1a72e445470be9adbc905639002eec4
Yusufazzam <168611383+yusufazzaamm@users.noreply.github.com>
2024-06-12 09:22:04 +0700

    Add files via upload

---
+++ remidi.py
@@ -0,0 +1,11 @@
+#apkdesain = ['corel' ,'canva' ,'photoshop' ,'adobe' ,'figma' ,'lightroom' ,'illustration' ,'Acrobat pro' ,'indesign' ,'sketch']
+#print (apkdesain[6])  
+
+#english = {'word': 'dunia','chair':'kursi','table':'meja','lamp':'lampu','door':'pintu','bag':'tas','fan':'kipas','bottle':'botol','sit':'duduk','tie':'dasi'}
+#print (english)
+
+def trapesium( a =5, b=7, T=10):
+    sum = 0.5 *(a+b)*T
+    print("sum:", sum)
+trapesium()   
+    
